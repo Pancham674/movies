61884cc5c4b53b11d41e85d7a30fa3ad0f337cb9
Pancham674 <pamela.p00@hotmail.de>
2026-06-15 01:19:40 +0200

    Added styling to Details

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -8,10 +8,13 @@ export default function Details() {
     const [isLoading, setIsLoading] = useState(true);
     const [movie, setMovie] = useState<MovieInfo>();
     const [error, setError] = useState("");
-    
-    const noImageFound = "https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg";
-    const imgPath = "https://image.tmdb.org/t/p/original/";
     const movId = Number(useParams().id);
+
+    const noImageFound = "https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg";
+    const imgPath = "https://image.tmdb.org/t/p/";
+    const w500 = "w500";
+    const w200 = "w200";
+    const og = "original";
 
     //Refetch the info everytime movId changes (basically everytime Details gets viewed)
     useEffect(() => {
@@ -35,59 +38,58 @@ export default function Details() {
             <div className="error-message">{error}</div> :
             isLoading ? 
             <p>Loading!!</p> :
-            <div className="details">
-                <img className="background-img" src={imgPath + movie!.backdrop_path}></img>
-                <img className="movie-poster" src={movie!.poster_path ? 
-                        imgPath + movie!.poster_path : noImageFound }>
-                </img>
-                <fieldset>
-                    <legend className="movie-title"><b>{movie!.title}</b></legend>
-                    { movie!.tagline && <h4 className="movie-subtitle"><i>"{movie!.tagline}"</i></h4> }
-                
-                    <p>General Information</p>
-                    <fieldset>
-                        <legend>Genres</legend>
-                            { movie!.genres.map(
-                                gen => <p className="tag" key={gen.id}>{gen.name}</p>) }
-                    </fieldset>
-                   
-                    <p>Status: {movie!.status}</p>
-                    <p>Release Date: {getReleaseDate(movie!.release_date)}</p>
-                    { movie!.runtime !== 0 && <p>Runtime: {`${Math.floor(movie!.runtime / 60)}H ${Number(movie!.runtime) % 60}`}M</p>}
-                    { movie!.homepage && <a href={movie!.homepage} target="_blank">Homepage Link...</a> }
-                   
-                    <fieldset>
-                        <legend>Production Companies:</legend>
-                        { movie!.production_companies.map(
-                            comp => comp.logo_path ? 
-                                <img key={comp.id} src={imgPath + comp.logo_path} alt={comp.name}></img> :
+            <>
+                <div className="details">
+                    <img className="poster" src={movie!.poster_path ? 
+                            imgPath + w500 + movie!.poster_path : noImageFound }>
+                    </img>
+                    <fieldset className="general-info">
+                        <legend className="movie-title"><b>{movie!.title}</b></legend>
+                        { movie!.tagline && <h4 className="movie-subtitle"><i>"{movie!.tagline}"</i></h4> }
+                    
+                        <fieldset>
+                            <legend>Genres</legend>
+                                { movie!.genres.map(
+                                    gen => <p className="tag" key={gen.id}>{gen.name}</p>) }
+                        </fieldset>
+                    
+                        <p>Status: {movie!.status}</p>
+                        <p>Release Date: {getReleaseDate(movie!.release_date)}</p>
+                        { movie!.runtime !== 0 && <p>Runtime: {`${Math.floor(movie!.runtime / 60)}H ${Number(movie!.runtime) % 60}`}M</p>}
+                        { movie!.homepage && <a href={movie!.homepage} target="_blank">Homepage Link...</a> }
+                    
+                        <fieldset>
+                            <legend>Production Companies:</legend>
+                            { movie!.production_companies.map(
+                                comp => comp.logo_path ? 
+                                <img key={comp.id} src={imgPath + w200 + comp.logo_path} alt={comp.name}></img> :
                                 <p key={comp.id}>{comp.name}</p>) 
-                        }
-                    </fieldset>
+                            }
+                        </fieldset>
 
-                    <fieldset>
-                        <legend>Production Countries:</legend>
-                        { movie!.production_countries.map(
-                            (coun, i) => <p className="tag" key={i}>{coun.name}</p>) }
-                    </fieldset>
+                        <fieldset>
+                            <legend>Production Countries:</legend>
+                            { movie!.production_countries.map(
+                                (coun, i) => <p className="tag" key={i}>{coun.name}</p>) }
+                        </fieldset>
 
-                    <fieldset>
-                        <legend>Available Languages:</legend>
-                            { movie!.spoken_languages.map(
-                                (lang, i) => <p className="tag" key={i}>{lang.english_name}</p>) }
-                    </fieldset>
+                        <fieldset>
+                            <legend>Available Languages:</legend>
+                                { movie!.spoken_languages.map(
+                                    (lang, i) => <p className="tag" key={i}>{lang.english_name}</p>) }
+                        </fieldset>
 
-                    { movie!.budget !== 0 && <p>Budget: { movie!.budget.toLocaleString("en-US") }$</p> }
-                    { movie!.revenue !== 0 && <p>Revenue: { movie!.revenue.toLocaleString("en-US") }$</p> }
-                    { (movie!.budget !== 0 && movie!.revenue !== 0) && <p>Profit: { (movie!.revenue - movie!.budget).toLocaleString("en-US") }$</p> }
-                </fieldset>
-                
-                <fieldset>
+                        { movie!.budget !== 0 && <p>Budget: { movie!.budget.toLocaleString("en-US") }$</p> }
+                        { movie!.revenue !== 0 && <p>Revenue: { movie!.revenue.toLocaleString("en-US") }$</p> }
+                        { (movie!.budget !== 0 && movie!.revenue !== 0) && <p>Profit: { (movie!.revenue - movie!.budget).toLocaleString("en-US") }$</p> }
+                    </fieldset>
+                </div>
+                <fieldset className="desc">
                     <legend>Description</legend>
                     <p>{movie!.overview}</p>
                 </fieldset>
-
-            </div>
+                <img className="background-img" src={imgPath + og + movie!.backdrop_path}></img>
+            </>   
         }
     </>)
 }
