71b057f1c25391546cf48b08c727a568143c0c03
Pancham674 <pamela.p00@hotmail.de>
2026-06-23 18:29:19 +0200

    ~ Info that has no value will no longer be displayed

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,11 @@
-import { getMovieDetails, getMoviesWithGenre } from "../services/api";
+import { getMovieDetails } from "../services/api";
 import type { MovieInfo, SpokenLanguages } from "../MovieInfo";
 import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../css/Details.css";
 
 export default function Details() {
+    const [isAnyInfoAvailable , setisAnyInfoAvailable] = useState(true);
     const [isLoading, setIsLoading] = useState(true);
     const [movie, setMovie] = useState<MovieInfo>();
     const [divBg, setDivBg] = useState("");
@@ -26,6 +27,11 @@ export default function Details() {
 
                 const bgImgDiv = mov!.backdrop_path ? imgPath + og + mov!.backdrop_path : "";
                 setDivBg(bgImgDiv);
+
+                if (!mov.overview && mov.budget == 0 && mov.revenue == 0 && !mov.homepage && mov.genres.length == 0 &&
+                     mov.production_companies.length == 0 && mov.production_countries.length == 0 && mov.spoken_languages.length == 0){
+                    setisAnyInfoAvailable(false);
+                }
             } catch (error: any) {
                 console.log(error);
                 setError(error.message);
@@ -45,7 +51,8 @@ export default function Details() {
                 <>
                     <div className="bg" style={{ backgroundImage: `url(${divBg})` }} />
                     <div className="details">
-                        <img className="poster" src={movie!.poster_path ? 
+                        <img className="poster"
+                            src={movie!.poster_path ? 
                                 imgPath + w500 + movie!.poster_path : noImageFound }>
                         </img>
 
@@ -55,8 +62,8 @@ export default function Details() {
                             <br />
                             <div className="other-info">
                                 <div>
-                                    <p>Status: {movie!.status}</p>
-                                    <p>Release Date: {getReleaseDate(movie!.release_date)}</p>
+                                    { movie!.status && <p>Status: {movie!.status}</p> }
+                                    { movie!.release_date && <p>Release Date: {getReleaseDate(movie!.release_date)}</p>}
                                     { movie!.runtime !== 0 && <p>Runtime: {`${Math.floor(movie!.runtime / 60)}H ${Number(movie!.runtime) % 60}`}M</p>}
                                 </div>
                                 <div>
@@ -65,36 +72,56 @@ export default function Details() {
                                     { (movie!.budget !== 0 && movie!.revenue !== 0) && <p>Profit: { (movie!.revenue - movie!.budget).toLocaleString("en-US") }$</p> }
                                 </div>
                             </div>
+
                             { movie!.homepage && <a href={movie!.homepage} target="_blank">Homepage Link...</a> }
+                            
+                            {
+                                movie!.genres.length > 0 &&
+                                <fieldset>
+                                    <legend>Genres</legend>
+                                        { movie!.genres.map(
+                                            (gen, i) => <p key={i}><Link to={`/genres/${gen.id}`} className="tag" key={gen.id}>{gen.name}</Link>,  </p>) }
+                                </fieldset>
+                            }
 
-                            <fieldset>
-                                <legend>Genres</legend>
-                                    { movie!.genres.map(
-                                        (gen, i) => <p key={i}><Link to={`/genres/${gen.id}`} className="tag" key={gen.id}>{gen.name}</Link>,  </p>) }
-                            </fieldset>
-                        
-                            <fieldset>
-                                <legend>Production Companies:</legend>
-                                { movie!.production_companies.map(
-                                    comp => <img key={comp.id} src={imgPath + w200 + comp.logo_path} alt={comp.name}></img>) 
-                                }
-                            </fieldset>
+                            {
+                                movie!.production_companies.length > 0 && 
+                                <fieldset>
+                                    <legend>Production Companies:</legend>
+                                    { movie!.production_companies.map(
+                                        comp => comp.logo_path ?
+                                                <img key={comp.id} src={imgPath + w200 + comp.logo_path}></img> :
+                                                <p>{comp.name}</p>) 
+                                    }
+                                </fieldset>
+                            }
 
-                            <fieldset>
-                                <legend>Production Countries:</legend>
-                                { movie!.production_countries.map(
-                                    (coun, i) => <p className="tag" key={i}>{coun.name}, </p>) }
-                            </fieldset>
+                            {
+                                movie!.production_countries.length > 0 && 
+                                <fieldset>
+                                    <legend>Production Countries:</legend>
+                                    { movie!.production_countries.map(
+                                        (coun, i) => <p className="tag" key={i}>{coun.name}, </p>) }
+                                </fieldset>
+                            }
 
-                            <fieldset>
-                                <legend>Available Languages:</legend>
-                                    { getLanguages(movie!.spoken_languages)}
-                            </fieldset>
+                            {
+                                movie!.spoken_languages.length > 0 && 
+                                <fieldset>
+                                    <legend>Available Languages:</legend>
+                                        <p>{ getLanguages(movie!.spoken_languages) }</p>
+                                </fieldset>
+                            }
                         </div>
 
                         <div className="desc">
                             <h4>Description</h4>
-                            <p>{movie!.overview}</p>
+                            <p>{movie!.overview ?
+                                    movie!.overview :
+                                    isAnyInfoAvailable ?
+                                        "No description available." :
+                                        "Hey TMDB, gimme a movie with NO relevant information!!!"}
+                            </p>
                         </div>   
                     </div>
                 </>
@@ -115,5 +142,5 @@ function getLanguages(languagesArray: SpokenLanguages[]): string {
         langs += `${lang.english_name}, `
     });
 
-    return langs.length != 0 ? langs.substring(0, langs.length - 2) : "none";
+    return langs.substring(0, langs.length - 2);
 }
